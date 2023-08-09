a93d84ab300eda511405d8cb06390be5b28d1aca
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-10 00:54:46 +0300

    Standart forma çevrildi

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-
-import { Field, Form, Formik } from 'formik';
-import './App.css';
+import { Field, Form, Formik } from "formik";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-   <h1>Sign Up</h1>
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={async (values) => {
-        await new Promise((r) => setTimeout(r, 500));
-        console.log(values);
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+       /*
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          console.log(values);
+          alert(JSON.stringify(values, null, 2));
+        }}
+        */
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input id="firstName" name="firstName" onChange={handleChange} placeholder="Jane" />
+            <br />
+            <br />
+            <label htmlFor="lastName">Last Name</label>
+            <input id="lastName" name="lastName" onChange={handleChange} placeholder="Doe" />
+            <br />
+            <br />
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              onChange={handleChange} 
+              placeholder="jane@acme.com"
+              type="email"
+            />
+            <br />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+
+        {/*
       <Form>
         <label htmlFor="firstName">First Name</label>
         <Field id="firstName" name="firstName" placeholder="Jane" />
-<br />
-<br />
+      <br />
+      <br />
         <label htmlFor="lastName">Last Name</label>
         <Field id="lastName" name="lastName" placeholder="Doe" />
         <br />
@@ -38,7 +67,8 @@ function App() {
         <br />
         <button type="submit">Submit</button>
       </Form>
-    </Formik>
+    */}
+      </Formik>
     </div>
   );
 }
